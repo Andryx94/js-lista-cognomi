2b4ce2ad343b4d5163f0b3f12b17be10dddb963b
Andrea <andryx1994@gmail.com>
2020-05-26 18:03:14 +0200

    Versione 1.0
    Ultimato esercizio

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ var cognomeUtente = "";
 
 while (!isNaN(cognomeUtente)) {
   cognomeUtente = prompt("Dimmi il tuo cognome");
-  cognomi.push(cognomeUtente);
-  cognomi.sort();
 }
+
+cognomi.push(cognomeUtente);
+cognomi.sort();
 
 alert("Ecco la lista ordinata dei cognomi: " + cognomi);
 alert("Il tuo cognome è in " + (cognomi.indexOf(cognomeUtente) +1) + "a posizione.");
